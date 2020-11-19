e7ba7e972d113051aa92dd1fa0131ae08cc5d11a
Albert Samuel Melo <asm.asm.albert@icloud.com>
2020-11-19 14:35:43 -0400

    Resultados Concreto

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Calculator from './src/views/Calculator';
-import TextPlaceHolder from "./src/views/Calculator"
+import CalcularConcreto from './src/views/CalcularConcreto'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Calculator/>
       <StatusBar style="auto" />
+      <CalcularConcreto/>
     </View>
   );
 }
